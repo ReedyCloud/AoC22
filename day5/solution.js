228b958c03434e69eeb773e0fdfe700732ffc734
Bartosz Ciach <bartoszciach99@gmail.com>
2022-12-05 10:44:38 +0100

    add day5

---
+++ day5/solution.js
@@ -0,0 +1,49 @@
+import { readFile } from "../utils.js";
+
+const data = readFile("./data.txt");
+/**
+ * 
+        [H]     [W] [B]            
+    [D] [B]     [L] [G] [N]        
+[P] [J] [T]     [M] [R] [D]        
+[V] [F] [V]     [F] [Z] [B]     [C]
+[Z] [V] [S]     [G] [H] [C] [Q] [R]
+[W] [W] [L] [J] [B] [V] [P] [B] [Z]
+[D] [S] [M] [S] [Z] [W] [J] [T] [G]
+[T] [L] [Z] [R] [C] [Q] [V] [P] [H]
+ 1   2   3   4   5   6   7   8   9 
+ */
+
+const cargo = [
+  [],
+  ["T", "D", "W", "Z", "V", "P"],
+  ["L", "S", "W", "V", "F", "J", "D"],
+  ["Z", "M", "L", "S", "V", "T", "B", "H"],
+  ["R", "S", "J"],
+  ["C", "Z", "B", "G", "F", "M", "L", "W"],
+  ["Q", "W", "V", "H", "Z", "R", "G", "B"],
+  ["V", "J", "P", "C", "B", "D", "N"],
+  ["P", "T", "B", "Q"],
+  ["H", "G", "Z", "R", "C"],
+];
+
+const cargo2 = JSON.parse(JSON.stringify(cargo));
+
+const moveCrate9000 = (from, to, howMany) => {
+  cargo[to].push(...cargo[from].splice(-howMany).reverse());
+};
+
+const moveCrate9001 = (from, to, howMany) => {
+  cargo2[to].push(...cargo2[from].splice(-howMany));
+};
+
+data.forEach((instruction) => {
+  const splitInstruction = instruction.split(" ");
+  moveCrate9000(splitInstruction[3], splitInstruction[5], splitInstruction[1]);
+  moveCrate9001(splitInstruction[3], splitInstruction[5], splitInstruction[1]);
+});
+
+const solution1 = cargo.map((row) => row[row.length - 1]).join("");
+const solution2 = cargo2.map((row) => row[row.length - 1]).join("");
+
+console.log(solution1, solution2);
